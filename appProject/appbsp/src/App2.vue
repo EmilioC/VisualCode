16d0605754c8bbec0d7d1c3b6f269d031b313d7c
EmilioC <emicorgul@gmail.com>
2018-05-25 23:27:42 +0200

    Test with first screen

---
+++ appProject/appbsp/src/App2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <meta name="viewport" content="width=device-width, initial-scale=1">      
+    <section >      
+        <router-link to="/formularioapt">Formulario </router-link>
+        <router-link to="/listarzona">Consulta</router-link>
+        <router-link to="/timeline">Timeline</router-link> 
+       <!--   <router-link to="/formularioapt">Formulario APT</router-link>
+        <router-link to="/resumen1">Resumen 1</router-link>
+        <router-link to="/boton1"> Boton 1</router-link>         
+        <router-link to="/boton2"> Boton 2</router-link>
+        <router-link to="/codeveloper"> codeveloper</router-link>                     
+        <router-link to="/listafuera">Lista tabla NAME</router-link>
+        <router-link to="/formulario">Formulario NAME</router-link>  -->     
+        <router-view></router-view> 
+    </section>
+    <ul>
+      <li><a href="https://www.danielprimo.io/podcast" target="_blank">WR</a></li>
+       <li><a href="https://github.com/EmilioC/VisualCode/tree/master/appProject/appbsp" target="_blank">Git</a></li>
+    </ul>   
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+    @import '/node_modules/bulma-extensions/bulma-timeline/dist/bulma-timeline.min.css';
+    @import '/node_modules/bulma/css/bulma.min.css';
+#app {
+  color: #2c3e50;
+  margin-top: 1%;
+  background-color: rgb(253, 253, 253);
+}
+
+</style>
--- appProject/appbsp/src/main.js
+++ appProject/appbsp/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import {routes} from './routes';
-import App from './App.vue';
+import App from './App2.vue';
 import VueFire from 'vuefire';
 import './firebase';
 
