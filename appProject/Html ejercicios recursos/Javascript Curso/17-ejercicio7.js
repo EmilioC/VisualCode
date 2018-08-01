e9ff380bc9a53e1115fa98e8175894a3a27bdfa1
EmilioC <emicorgul@gmail.com>
2018-08-01 22:29:18 +0200

    Test with Javascript

--- appProject/Html ejercicios recursos/Javascript Curso/17-ejercicio7.js
+++ appProject/Html ejercicios recursos/Javascript Curso/17-ejercicio7.js
@@ -2,3 +2,17 @@
 
 // Tabla de multiplicar de un numero introducido por pantalla
 
+var numero = parseInt (prompt ("Insert coin",1));
+
+document.write ("<h1>Tabla del "+numero+"</h1>");
+
+for (var i = 1; i <= 10; i++ ){
+	document.write (i+ " x "+numero+ " = "+(i*numero)+"<br/>");
+	}
+// Todas las tablas de multiplicar
+for(var c = 1; c <= 15; c++){
+	document.write("<h1>Tabla del "+c+"</h1>");
+	for(var i = 1; i <= 10; i++){
+		document.write(i+" x "+c+" = "+(i*c)+"<br/>");
+	}
+}
